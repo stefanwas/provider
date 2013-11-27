78fe452614bdd8226029b1fbd7198cf19dbdbbc3
stefanwas <swtemp001@gmail.com>
2013-11-27 10:26:59 +0100

    changes to css

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -29,7 +29,38 @@ controllers.FilterController = function($scope, $resource) {
 							{name: "Philipines", selected: false},
 							{name: "Vietnam", selected: false}]
 						}
-						];
+	];
+	
+	$scope.divisions = [
+	                    {name: "Power Products",
+	                    	selected: false, 
+	                    	items: [
+	                    	        {name: "Wiring Accessories", selected: false}, 
+	                    	        {name: "Low Voltage", selected: false},
+	                    	        {name: "Transformers", selected: false}
+	                    	 ]
+	                    },
+	                    {name: "Low Voltage Products",
+	                    	selected: false, 
+	                    	items: [
+	                    	        {name: "High Voltage", selected: false}, 
+	                    	        {name: "Medium Voltage", selected: false},
+	                    	        {name: "Transformers", selected: false}
+	                    	        ]
+	                    },
+              	        {name: "Process Automation",
+           	        	selected: false,
+           	        	items: [
+        	        	        {name: "Control Technologies", selected: false},
+        	        	        {name: "Full Service", selected: false},
+        	        	        {name: "Industry Solutions", selected: false},
+        	        	        {name: "Marine and Cranes", selected: false},
+        	        	        {name: "Measurement Products", selected: false},
+        	        	        {name: "Service", selected: false},
+        	        	        {name: "Turbocharging", selected: false}
+        	        	        ]
+               	        }
+    ];
 	
 	
 
